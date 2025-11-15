c52c138cb11ce6b744f443aba62a60ad84ecb01c
armandoeconomia <armandoeconomias@gmail.com>
2025-11-15 15:17:53 -0300

    MODIFICACIÓN DEL SEED

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,7 +2,7 @@ class Doctor < ApplicationRecord
   belongs_to :user
   belongs_to :medical_institute
 
-  has_many :appoinments
+  has_many :appointments
   has_many :medical_histories
   has_many :calendars
   has_many :hours, through: :calendars
