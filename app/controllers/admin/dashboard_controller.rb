b3fec931d49301736d77c5f4037c5734037fe421
Olger Cascante <olgerca100@gmail.com>
2025-11-18 20:13:07 -0600

    Avance User

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
-class Admin::DashboardController < ApplicationController
+class Admin::DashboardController < Admin::BaseController
   def show
+    @total_users = User.count
+    #@total_patients = Patient.count
+    #@total_doctors = Doctor.count
+    #@appointments_today = Appointment.where(date: Date.today).count
+    #@invoices_count = Invoice.count
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,22 +1,51 @@
-class Admin::UsersController < ApplicationController
+class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: %i[show edit update destroy]
+
   def index
+    @users = User.all
+  end
+
+  def show;
   end
 
   def new
+    @user = User.new
   end
+
+  def edit; end
 
   def create
-  end
-
-  def show
-  end
-
-  def edit
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_user_path(@user), notice: "User created"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "User updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @user.destroy
+    redirect_to admin_users_path, notice: "User deleted"
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :email, :password, :name, :last_name, :phone_number,
+      :birthday, :address, :identification, :gender, :admin
+    )
   end
 end
