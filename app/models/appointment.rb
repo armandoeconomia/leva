3ca08c6d0d3a39ac9eca00bcba2bc1b215e5bf55
Olger Cascante <olgerca100@gmail.com>
2025-12-06 13:04:38 -0600

    Agreglo de temas varios

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -11,4 +11,24 @@ class Appointment < ApplicationRecord
   validates :doctor, presence: true
   validates :hour, uniqueness: { scope: [:date, :doctor_id],
                                  message: "Ya existe una cita para este doctor en esa fecha y hora"}
+  validate :hour_within_business_hours
+
+  private
+
+  def hour_within_business_hours
+    return if hour.blank?
+
+    seconds = hour.seconds_since_midnight
+    earliest = 8.hours
+    latest = 17.hours
+
+    unless seconds.between?(earliest, latest)
+      errors.add(:hour, "debe estar entre las 08:00 y las 17:00")
+      return
+    end
+
+    unless (seconds % 1.hour).zero?
+      errors.add(:hour, "debe configurarse en intervalos de 1 hora")
+    end
+  end
 end
